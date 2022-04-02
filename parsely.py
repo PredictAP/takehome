3b34ebb6f3840711ed44e2f7e0b8e92377a1f23c
fahali <fahim.ali.204@gmail.com>
2022-04-02 18:22:10 -0400

    F# cleanup

--- parsely.py
+++ parsely.py
@@ -1,9 +1,20 @@
 import os
+import sys
 import json
 
 
+# Shared constants
 INDEX_FILENAME = "parsely_index.json"
-FILESIZE_FUZZ_FACTOR = 0.1
+
+PATH = "path"
+NAME = "name"
+SIZE = "size"
+TYPE = "type"
+
+BYTE = "B"
+KILO = "K"
+MEGA = "M"
+GIGA = "G"
 
 
 def build_index(directory, files=[]):
@@ -12,12 +23,13 @@ def build_index(directory, files=[]):
             if item.is_dir():
                 build_index(item.path, files)
             else:
+                filename, filetype = os.path.splitext(item.name)
                 files.append(
                     {
-                        "path": os.path.abspath(item.path),
-                        "name": item.name.split(".")[0],
-                        "size": item.stat().st_size,
-                        "type": item.name.split(".")[1],
+                        PATH: os.path.abspath(item.path),
+                        NAME: filename,
+                        SIZE: item.stat().st_size,
+                        TYPE: filetype,
                     }
                 )
 
@@ -25,12 +37,14 @@ def build_index(directory, files=[]):
 
 
 def persist_index(index):
-    with open(INDEX_FILENAME, "w") as file:
+    WRITE_FLAG = "w"
+    with open(INDEX_FILENAME, WRITE_FLAG) as file:
         json.dump(index, file)
 
 
 def load_index():
-    with open(INDEX_FILENAME, "r") as file:
+    READ_FLAG = "r"
+    with open(INDEX_FILENAME, READ_FLAG) as file:
         index = json.load(file)
 
     return index
@@ -41,46 +55,44 @@ def index(args):
 
 
 def search_filename(name, filtered):
-    return [file for file in filtered if file["name"].find(name) != -1]
+    return [file for file in filtered if file[NAME].find(name) != -1]
 
 
 def search_filesize(size, scale, filtered):
-    if scale != "B" and scale != "K" and scale != "M" and scale != "G":
-        print(
+    scale_table = {BYTE: 1, KILO: 1000, MEGA: 1000000, GIGA: 1000000000}
+    if not scale in scale_table:
+        sys.exit(
             "Invalid scale provided. Please try again with one of the "
-            + "following: B for bytes, K for kilobytes, M for megabytes, and G "
-            + "for gigabytes."
+            + f"following: {BYTE} for bytes, {KILO} for kilobytes, {MEGA} for "
+            + f"megabytes, and {GIGA} for gigabytes."
         )
-        return
 
-    scale_table = {"B": 1, "K": 1000, "M": 1000000, "G": 1000000000}
+    FILESIZE_FUZZ_FACTOR = 0.1
     size_query = size * scale_table[scale]
     floor = size_query - (size_query * FILESIZE_FUZZ_FACTOR)
     ceiling = size_query + (size_query * FILESIZE_FUZZ_FACTOR)
 
-    return [
-        file for file in filtered if file["size"] >= floor and file["size"] <= ceiling
-    ]
+    return [file for file in filtered if file[SIZE] >= floor and file[SIZE] <= ceiling]
 
 
 def search_filetype(type, filtered):
-    return [file for file in filtered if file["type"] == type]
+    return [file for file in filtered if file[TYPE] == type]
 
 
 def print_results(filtered):
     if len(filtered) == 0:
-        print("No results found for the given search query.")
-    else:
-        print("Found the following result(s) from the given search query.")
-        for item in filtered:
-            print(f"{item['name']}.{item['type']} located at {item['path']}")
+        sys.exit("No results found for the given search query.")
+
+    print("Found the following result(s) from the given search query.")
+    for item in filtered:
+        print(f"{item[NAME]}{item[TYPE]} located at {item[PATH]}")
 
 
 def search(args):
     if args.name == None and args.size == None and args.type == None:
-        print("Specify at least one search function and corresponding criteria.")
+        sys.exit("Specify at least one search function and corresponding criteria.")
     if not os.path.isfile(INDEX_FILENAME):
-        print(
+        sys.exit(
             "There is no existing index file. Run this program with the index "
             + "command and try your search again."
         )
@@ -109,47 +121,49 @@ def build_parser():
 
     index_parser = subparsers.add_parser(
         "index",
-        help="""Index a directory tree.""",
+        help="Index a directory tree.",
     )
     index_parser.add_argument(
         "directory",
+        nargs="?",
         metavar="DIRECTORY",
-        help="""Index the given directory. Must supply a directory path as a 
-        positional argument.""",
+        default=os.getcwd(),
+        help="Index the given directory. If no path is supplied, will index "
+        + "the current working directory.",
     )
-    index_parser.set_defaults(func=index)
+    index_parser.set_defaults(handler=index)
 
     search_parser = subparsers.add_parser(
         "search",
-        help="""Perform a search query.""",
+        help="Perform a search query.",
     )
     search_parser.add_argument(
         "-n",
         "--name",
         metavar="FILENAME",
-        help="""Search for files by filename. Must supply part or all of a 
-        filename as a positional argument to this flag.""",
+        help="Search for files by filename. Must supply part or all of a "
+        + "filename as a positional argument to this flag.",
     )
     search_parser.add_argument(
         "-s",
         "--size",
         nargs=2,
         metavar=("FILESIZE", "SCALE"),
-        help="""Search for files by filesize. Must supply the filesize as two 
-        positional arguments to this flag: the first argument is the 
-        quantitative size, and the second argument is the scale. For example, 
-        an argument of '4 M' corresponds to a size of 4 megabytes. Searching is 
-        only supported for byte (B), kilobyte (K), megabyte (M), and gigabyte 
-        (G) scales.""",
+        help="Search for files by filesize. Must supply the filesize as two "
+        + "positional arguments to this flag: the first argument is the "
+        + "quantitative size, and the second argument is the scale. For "
+        + "example, an argument of '4 M' corresponds to a size of 4 megabytes. "
+        + f"Searching is only supported for byte ({BYTE}), kilobyte ({KILO}), "
+        + f"megabyte ({MEGA}), and gigabyte ({GIGA}) scales.",
     )
     search_parser.add_argument(
         "-t",
         "--type",
         metavar="FILETYPE",
-        help="""Search for files by filetype. Must supply the filetype as a 
-        positional argument to this flag.""",
+        help="Search for files by filetype. Must supply the filetype as a "
+        + "positional argument to this flag.",
     )
-    search_parser.set_defaults(func=search)
+    search_parser.set_defaults(handler=search)
 
     return parser
 
@@ -157,4 +171,4 @@ def build_parser():
 if __name__ == "__main__":
     parser = build_parser()
     args = parser.parse_args()
-    args.func(args)
+    args.handler(args)
