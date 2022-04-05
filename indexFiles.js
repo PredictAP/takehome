99c9cc1f6d5c18c6ee268072bf83137e5ee3d10c
ckendig <ckendig2@gmail.com>
2022-04-05 13:13:50 -0400

    removed commented out code

--- indexFiles.js
+++ indexFiles.js
@@ -1,38 +1,6 @@
 const fs = require('fs');
 const path = require('path');
 
-// const indexFiles = async () => {
-//     let indexDirectory = [];
-//     await fs.readdir('./test_data', (err, files) => {
-//         if(err) {
-//             console.error('there was an error reading the files', err);
-//             return {message: 'there was an error reading the files', error: err, status: 'failed'};
-//         }
-
-//         for (let i = 0; i < files.length; i++) {
-
-//             const filePath = path.join('./test_data', files[i]);
-//             fs.stat(filePath, (error, stat) => {
-//                 if(stat.isFile()) {
-//                     console.log(filePath, ' is a file');
-//                     indexDirectory.push({
-//                         fileName: files[i],
-//                         filePath: filePath,
-//                         fileSize: stat.size,
-//                         contentType: path.extname(files[i])
-//                     })
-//                 }
-
-//                 if(stat.isDirectory()) {
-//                     console.log(filePath, ' is a directory');
-//                 }
-//             })
-//         };
-//     });
-//     console.log('indexDirectory >>> ', indexDirectory);
-//     return indexDirectory
-// }
-
 const indexFiles = (directoryPath, arrayOfFiles) => {
 
         const files = fs.readdirSync(directoryPath);
